c58796dbda03ba5c0eaec63f0fa320b22f42ec9d
thunguyen1919 <thurapper1919@gmail.com>
2021-11-09 22:10:56 +0700

    Complete the Checkout test cases

--- source/TestSuites/test_Checkout.py
+++ source/TestSuites/test_Checkout.py
@@ -16,6 +16,19 @@ class TestCheckout():
         yield
         self.driver.close()
 
+    @pytest.fixture()
+    def login(self, setup):
+        self.driver.get('http://supermarket-tws.somee.com/')
+        self.driver.find_element(By.LINK_TEXT, 'Login').click()
+        self.driver.execute_script('window.scrollTo(0,300)')
+        self.driver.find_element(By.ID, 'UserName').send_keys('ThuNguyen')
+        self.driver.find_element(By.ID, 'Password').send_keys('asd@12345')
+        self.driver.find_element(By.ID, 'LoginButton').click()
+        wait = WebDriverWait(self.driver, 20)
+        wait.until(EC.text_to_be_present_in_element((By.XPATH, '//span[@id="Header_LoginViewHeader_LoginName1"]'),
+                                                    "ThuNguyen"))
+        self.driver.get_screenshot_as_file('.\\Results\\demo.png')
+
     def test_buy_products_when_not_logged_in(self, setup):
         self.driver.get('http://supermarket-tws.somee.com/')
         self.driver.find_element(By.LINK_TEXT, 'SHOP NOW').click()
@@ -40,5 +53,29 @@ class TestCheckout():
         WebDriverWait(self.driver, 20).until(EC.text_to_be_present_in_element(
             (By.TAG_NAME,'h2'),'LOGIN FORM'))
         self.driver.execute_script('window.scrollBy(0,280)')
+
+    def test_buy_products_when_not_logged_in(self, setup, login):
+        self.driver.find_element(By.LINK_TEXT, 'SHOP NOW').click()
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(
+            (By.CSS_SELECTOR, "ol[class='breadcrumb breadcrumb1 animated wow slideInLeft'] li[class='active']")))
+        self.driver.execute_script('window.scrollBy(0,280)')
+        #Add item Fry Pan
+        self.driver.find_element(By.XPATH,'//input[@id="MainContent_ItemList_ProductsListView_ctrl0_ButtonAddToCart_0"]').click()
+
+        # Add item Toor Dal
+        WebDriverWait(self.driver,20).until(EC.visibility_of_element_located((By.XPATH,'//a[@id="MainContent_ItemList_ProductsListView_ctrl0_HyperLink1_2"]')))
+        self.driver.execute_script('window.scrollBy(0,280)')
+        self.driver.find_element(By.XPATH,'//input[@id="MainContent_ItemList_ProductsListView_ctrl0_ButtonAddToCart_2"]').click()
+
+        #Go to Checkout
+        self.driver.find_element(By.XPATH,'//i[@class="fa fa-cart-arrow-down"]').click()
+        WebDriverWait(self.driver,20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "ol[class='breadcrumb breadcrumb1'] li[class='active']")))
+        self.driver.execute_script('window.scrollBy(0,180)')
+
+        #Click the Checkout button to go to Review Your Order & Complete Checkout page
+        self.driver.find_element(By.ID, 'MainContent_ButtonCheckout').click()
+        WebDriverWait(self.driver, 20).until(EC.text_to_be_present_in_element(
+            (By.TAG_NAME,'h2'),'Review Your Order & Complete Checkout'))
+
 
 
